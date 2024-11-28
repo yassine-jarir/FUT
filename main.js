6e98e1533f6d76dcad57b7ca25fb22a2ea57c1a8
Yassine <yassinejarir7@gmail.com>
2024-11-28 16:17:38 +0100

    check position

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ const boxs = document.querySelectorAll(".box")
 
 const sugg = document.querySelector(".sugg")
 
- 
+
 
 
 const arr = []
- 
+ const suggArr = []
 if (arr.length == 0) {
     Array.from(boxs).map((item, i) => {
       return item.innerHTML = `<img class="boxImg " src="./src/assets/img/badge_gold.webp" alt="" >
@@ -37,23 +37,79 @@ if (arr.length == 0) {
 }
     
 butnSubmit.addEventListener("click",(e) => {
-    e.preventDefault()    
-    arr.push({
-        nameInput :nameInput.value ,
-        positionInput: positionInput.value,
-        nationalInput: nationalInput.value,
-        imageInput: imageInput.value,
-        shootingInput: shootingInput.value,
-        dribblingInput: dribblingInput.value,
-        clubInput: clubInput.value,
-        defendingInput: defendingInput.value,
-        physicalInput: physicalInput.value,
-        paceInput: paceInput.value,
-        passingInput: passingInput.value       
-     })
-     handlePlayerBox()
+    e.preventDefault() 
+
+    const playerExists = arr.some((existingPlayer) =>  existingPlayer.nameInput == nameInput.value );
+    const existingPlayer = arr.find((player) => 
+    {
+        if(player.nameInput === nameInput.value){
+            return player
+        }
+    }     
+    
+    
+);
+  
+console.log("existingPlayer", existingPlayer)
+if(playerExists){
+console.log("player already exist");
+}
+     if (!playerExists) {
+        console.log("sd",positionInput.value)
+         // check position if its empty 
+
+
+
+      const gktest =   Array.from(boxs).filter((box, i) => console.log(`${i}`,box))
+      console.log(gktest)
+        
+      arr.push({
+            nameInput :nameInput.value ,
+            positionInput: positionInput.value,
+            nationalInput: nationalInput.value,
+            imageInput: imageInput.value,
+            shootingInput: shootingInput.value,
+            dribblingInput: dribblingInput.value,
+            clubInput: clubInput.value,
+            defendingInput: defendingInput.value,
+            physicalInput: physicalInput.value,
+            paceInput: paceInput.value,
+            passingInput: passingInput.value,
+            game: "basic"
+         })
+
+         const checkPlayerExist = gktest.filter((box) =>
+         {
+            const boxCheck = document.querySelector(`.${positionInput.value}`)
+return boxCheck
+         })
+        
+         
+         // get box depend on position and check if its null 
+         console.log("checkPlayerExist", checkPlayerExist)
+         
+        }
+    else if(playerExists && existingPlayer.game == "basic"){
+        existingPlayer.game = "remplacment"
+        const sugPlayers = Array.from(arr).filter((player) => {
+          if(player.game == "remplacment"){
+            return player
+          }
+        }
+        )
+       console.log("sugPlayers", sugPlayers)
+       return
+    }else{
+       console.log("done")
+    }
+    
+     console.log(arr)
     }
 ) 
+
+// 1 check the player card 
+// 2 check the player changement 
+// 3 add the player 
 
 function handlePlayerBox(){
     console.log(Array.from(boxs).length)
@@ -76,6 +132,9 @@ function handlePlayerBox(){
                         <img class="w-[76px]" src="https://cdn.sofifa.net/players/238/794/25_120.png" alt="">
                       </div>
                     </div>
+                    you dont get it i when i submit the form it have to do some checks so if 
+                    the player is in the player card add it to the player card if its not 
+                    add it to the changenent card 
                     <div class="flex flex-col justify-center items-center w-full">
                       <h4 class="font-bold">wd</h4>
                       <div class="flex w-full flex justify-around">
@@ -119,11 +178,12 @@ function handlePlayerBox(){
                     </div>
                   </div>
                 `;
-        }
+            }
     }
         )
      }
     playersBox.innerHTML = ""
+
 arr.map((item) => {
   return  playersBox.innerHTML += `<div class="box">
                         <img class="boxImgq h-[315px]" src="./src/assets/img/badge_gold.webp" alt="">
@@ -133,7 +193,7 @@ arr.map((item) => {
                                     <span>81</span>
                                     <span class="font-medium">${item.positionInput}</span>
                                     <img class="w-[30px]" src="https://cdn.sofifa.net/flags/br.png" alt="">
-                                    <img class="w-[30px]" src="https://cdn.sofifa.net/meta/team/3468/120.png" alt="">
+                                    <i class="w-[30px]" src="https://cdn.sofifa.net/meta/team/3468/120.png" alt="">
                                  </div>
                                 <div>
                                    <img class="w-[76px]" src="https://cdn.sofifa.net/players/238/794/25_120.png" alt="">
